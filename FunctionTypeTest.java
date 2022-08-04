249c401073cb109a3a79a38a345a68eb5df0aafe
SINCHIT BATHAM <sinchit417@gmail.com>
2022-08-04 09:57:12 +0530

    Update FunctionTypeTest.java

--- FunctionTypeTest.java
+++ FunctionTypeTest.java
@@ -1,22 +1,22 @@
 
-public class FunctionTypeTest {
+public class FunctionTesting {
 	public static void main(String[] args) {
 		
 		
 	}
-	void fun1()
+	void function1()
 	{
-		System.out.println("fun1 is doing some activity");
+		System.out.println("function1 is doing some activity");
 		
 	}
-     void fun2(int x, float y, double z)
+     void function2(int x, float y, double z)
      {
-    	 System.out.println("fun2 is doing some activity");
+    	 System.out.println("function2 is doing some activity");
     	 
      }
-     double fun3(char x, int y, float z)
+     double function3(char x, int y, float z)
      {
-    	 System.out.println("fun3 is doing some activity");
-    	 return 45.55;
+    	 System.out.println("function3 is doing some activity");
+    	 return 100.50;
      }
 }
